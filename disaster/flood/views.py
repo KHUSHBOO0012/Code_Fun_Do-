c23937728df72cfd75453d39bbeb66673b58877b
sharifa2708 <sanjay5803@gmail.com>
2018-10-26 21:16:49 +0530

    Completely functional web app

--- disaster/flood/views.py
+++ disaster/flood/views.py
@@ -1,8 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from .models import *
+from . import predict
 
 def index(request):
      return render(request, 'flood/index.html')
 def prediction(request):
+     predict.main(request.POST['regions'])
      return render(request, 'flood/predict.html')
